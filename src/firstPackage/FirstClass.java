3e57f310e54fd310deacfec207f3c708a086cbd8
Gdagdelen <gokaydagdelen@gmail.com>
2020-11-07 14:49:45 -0300

    Second commit from loak master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,8 +3,12 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("First commit from local master");
+		
+		System.out.println("First commit from the local master");
 
+		System.out.println("Second commit from the local master");
+		
+		
 	}
 
 }
